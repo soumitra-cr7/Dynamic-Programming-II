8295dd3647fe5efe5ac099dfdb467e987537260b
Sowmittro Singha <soumitrasingha0070@gmail.com>
2026-06-17 20:23:00 +0600

    Add files via upload

---
+++ max_CC.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxCoins(vector<int>& coins, int amount)
+{
+    int n = coins.size();
+
+    vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
+
+    for (int i = 0; i <= n; i++)
+    {
+        dp[i][0] = 1;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= amount; j++)
+        {
+            if (coins[i - 1] <= j)
+            {
+                dp[i][j] = max(dp[i - 1][j], 1 + dp[i][j - coins[i - 1]]);
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+
+    if (dp[n][amount] >= INT_MAX - 1)
+    {
+        return -1;
+    }
+    return dp[n][amount];
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int amount;
+    cin >> amount;
+
+    vector<int> coins(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> coins[i];
+    }
+
+    int result = maxCoins(coins, amount);
+    
+    cout << result << endl;
+    return 0;
+}
